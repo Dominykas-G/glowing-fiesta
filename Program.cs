b00f2f718f6d38d03010cf9afbcb405a923451a5
Dominykas Gudavičius <gudavicius.dominykas@gmail.com>
2023-02-09 10:25:38 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Soft{
+       public static void write()
+        {
+            Console.WriteLine("This is from another class\a");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+           Soft.write();
+           Console.WriteLine("Hello, World!");
+        }
+    }
+}
